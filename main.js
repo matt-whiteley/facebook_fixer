f0e9358985669cc907ac2bada0ef4060ac60ab12
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2016-06-28 14:05:30 +0100

    background image

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 var problem = document.getElementById("globalContainer");
 problem.parentNode.removeChild(problem);
+
+// http://funnyweddingpicture.com/wp-content/uploads/2016/03/amazing-lake-landscape-to-use-as-wallpaper.jpg
